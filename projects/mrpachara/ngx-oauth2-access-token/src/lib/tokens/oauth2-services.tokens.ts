f4dc43fd628b848f2644873c3daf7a1617d1552d
Pachara Tinamas <mrpachara@gmail.com>
2023-06-07 18:32:27 +0700

    custom service can use fullconfig

--- projects/mrpachara/ngx-oauth2-access-token/src/lib/tokens/oauth2-services.tokens.ts
+++ projects/mrpachara/ngx-oauth2-access-token/src/lib/tokens/oauth2-services.tokens.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 
 import { AccessTokenResponse } from '../types';
 import { AccessTokenService } from '../access-token.service';
+import { AuthorizationCodeService } from '../authorization-code.service';
 
 export const ACCESS_TOKEN_SERVICES = new InjectionToken<AccessTokenService[]>(
   'access-token-services',
@@ -11,6 +12,13 @@ export const ACCESS_TOKEN_SERVICES = new InjectionToken<AccessTokenService[]>(
     factory: () => [],
   },
 );
+
+export const AUTHORIZATION_CODE_SERVICES = new InjectionToken<
+  AuthorizationCodeService[]
+>('authorization-code-services', {
+  providedIn: 'root',
+  factory: () => [],
+});
 
 export const RENEW_ACCESS_TOKEN_SOURCE = new InjectionToken<
   Observable<AccessTokenResponse>
