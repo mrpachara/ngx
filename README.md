1a2737ea8f5c3c60e62578800482b0f3e0040316
Pachara Tinamas <mrpachara@gmail.com>
2025-03-06 13:18:54 +0700

    refector: upgrade to Angular 19.2

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Ngx
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 19.2.1.
 
 ## Development server
 
--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    imports: [CommonModule, RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'demo';
